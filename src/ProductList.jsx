7e2a40c1a093d1de2a3028dbbb0c59dbba23eba0
McSho <McSho@github.com>
2024-09-25 05:14:08 -0400

    Centered Category

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -327,7 +327,7 @@ function ProductList() {
                 <div className="product-grid">
                     {plantsArray.map((category, index) => (
                         <div key={index}>
-                            <h1>{category.category}</h1>
+                            <h1 className="category-title">{category.category}</h1>
                             <div className="product-list">
                                 {category.plants.map((plant, plantIndex) => (
                                     <div className="product-card" key={plantIndex}>
