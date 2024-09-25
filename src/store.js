9b34d285f5f156fc54f76cced70c714c3088c33a
McSho <McSho@github.com>
2024-09-25 05:03:22 -0400

    Task 4 and Task 5

--- src/store.js
+++ src/store.js
@@ -1,9 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
 
- const store = configureStore({
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './CartSlice';  // Ensure the path to CartSlice is correct
+
+// Configure the Redux store
+const store = configureStore({
     reducer: {
-        cart: cartReducer,
+        cart: cartReducer,  // Associate the cart reducer with the 'cart' key in the state
     },
 });
-export default store
+
+export default store;
