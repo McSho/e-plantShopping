41a9d9e7dcaf26cce0dd5f502f96a118b99d6848
McSho <McSho@github.com>
2024-09-25 05:05:29 -0400

    Task 6 Set up global store

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';  // Ensure Provider is imported
-import store from './store';  // Import the store
+import { Provider } from 'react-redux';  // Import Provider from react-redux
+import store from './store';  // Import the Redux store
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
-  <Provider store={store}>  {/* Wrap the app with Provider */}
+  <Provider store={store}>  {/* Wrap the app with Provider and pass the store */}
     <App />
   </Provider>,
   document.getElementById('root')
